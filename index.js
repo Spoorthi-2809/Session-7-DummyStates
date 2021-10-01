7bbdf5d332cab6a03d4a080e655d97e1b4bacba8
Spoorthi Sudhakar <90171439+Spoorthi-2809@users.noreply.github.com>
2021-10-01 12:47:14 +0530

    Update index.js

--- index.js
+++ index.js
@@ -4,14 +4,65 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+class MyForm extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      phonenumber: ''
+    };
+  }
+  mySubmitHandler = (event) => {
+    event.preventDefault();
+    let phonenumber = this.state.phonenumber;
+    if (!Number(phonenumber)) {
+      alert("Your phone number must be a number");
+    }
+    if(!Number == 10(phonenumber)) {
+      alert("Your phone number should be 10 digits");
+    }
+  }
+  myChangeHandler = (event) => {
+    let nam = event.target.name;
+    let val = event.target.value;
+    let err = '';
+    if (nam === "age") {
+      if (val !="" && !Number(val)) {
+        err = <strong>Your age must be a number</strong>;
+      }
+    }
+    this.setState({errormessage: err});
+    this.setState({[nam]: val});
+    if (!Number(val)) {
+        alert("Your phone number must be a number");
+      }
+    if(!Number(val)) {
+      alert("Your phone number should be 10 digits");
+    }
+      this.setState({[nam]: val});
+    }
+  render() {
+    return (
+      <form>
+        <h1>User Form</h1>
+        <p>Phone Number:</p>
+        <input
+        type="text"
+        onChange={this.myChangeHandler}
+        />
+        <br/>
+        <br/>
+        <p> Has Data Pack</p>
+        <input type="radio" value="Has Data Pack" name="Data" /> Yes 
+        <br/>
+        <br/>
+        <input type="radio" value="Has Data Pack" name="Data" /> No
+        <br/>
+        <br/>
+        <input
+        type="submit"
+        />
+      </form>
+    );
+  }
+}
+ReactDOM.render(<MyForm />, document.getElementById('root'));
